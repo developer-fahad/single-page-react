daa4e6c2ea5da97bbdfe3d92d1fc31282d428363
DeveloperFahad <developerfahad.me@gmail.com>
2024-10-07 01:36:43 +0600

    Added a beatiful footer and responsive for all devices

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -13,7 +13,13 @@ const Hero = () => {
       <div className="hero-content text-neutral-content text-center">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Always be happy!</h1>
-          
+          <p className="mb-5 md:text-xl text-lg">
+            Embrace every moment with positivity and joy. Happiness is not just
+            a destination but a journey that begins with a mindset. Whether it's
+            through growth, learning, or connection, let's cultivate an
+            environment where every step forward brings a sense of fulfillment
+            and peace.
+          </p>
           <button className="btn bg-cyan-600 border-none hover:bg-cyan-700 md:text-xl text-lg text-white">
             Get Started
           </button>
